392ff590f664a4e321bca36d25c7d3d95c55cf85
RAM111111 <r.r.99@windowslive.com>
2021-12-22 14:41:41 +0300

    Initial Commit

---
+++ GET People/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GET People
+//
+//  Created by R on 18/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
